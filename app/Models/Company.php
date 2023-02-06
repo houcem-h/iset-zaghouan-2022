84b1f39b313f4f820aefa66651df3aeabcd5d849
houcem <hedhoucem@gmail.com>
2023-02-06 15:03:56 +0100

    eloquent relationships

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Listing;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Company extends Model
 {
     use HasFactory;
+
+    public function listings()
+    {
+        return $this->hasMany(Listing::class);
+    }
 }
--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Company;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Listing extends Model
 {
     use HasFactory;
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
