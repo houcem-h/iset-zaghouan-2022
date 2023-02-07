4e702673a918fd55dc2233a829a8f7b8aff84e3c
houcem <hedhoucem@gmail.com>
2023-02-07 10:34:34 +0100

    other method of seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,11 +17,28 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(5)->create();
         \App\Models\Company::factory(7)->create();
-        \App\Models\Listing::factory(20)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Listing::create([
+            'title' => 'Senior Laravel Developper',
+            'tags' => 'Laravel, PHP',
+            'position_number' => 1,
+            'job_location' => 'Lagos, Nigeria',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
+            'company_id' => 1
+        ]);
+        Listing::create([
+            'title' => 'Full-stack Developper',
+            'tags' => 'Laravel, PHP, Vue.js, javascript',
+            'position_number' => 2,
+            'job_location' => 'Turkey, Istanbul',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
+            'company_id' => 2
+        ]);
+        Listing::factory(20)->create();
     }
 }
